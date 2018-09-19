117cb4a2e8306f939b60c790e5965406edd84d21
Daniel Ricart Sánchez <danielillu@gmail.com>
2018-09-20 00:22:17 +0200

    added calibrate servo code

---
+++ calibrateServo/calibrateServo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+#define MIN 25
+#define MAX 180
+
+Servo myservo; 
+int angle = 179;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+pinMode(8, INPUT_PULLUP);
+myservo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (angle > MIN && angle < MAX) {
+    myservo.write(angle);
+    angle--;
+    Serial.println(angle);
+  }
+  delay(200);  
+}
