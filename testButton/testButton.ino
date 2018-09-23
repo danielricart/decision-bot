0864682a5110cd58ccf4591ae04f454bb5bb6f44
Daniel Ricart <daniel.ricart@scmspain.com>
2018-09-23 02:39:00 -0400

    added test button

---
+++ testButton/testButton.ino
@@ -0,0 +1,17 @@
+#define button 8
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(button, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(button)) {
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off (LOW is the voltage level)
+  }
+  else {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)  
+  }
+}
